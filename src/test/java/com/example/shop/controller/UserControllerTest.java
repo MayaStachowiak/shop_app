7a68e942361f8e6037c9fd569887d2ec2270a643
NBMARIASW11\Maria Stachowiak <mariastachowiak87@gmail.com>
2024-06-23 16:57:20 +0200

    add UserControllerTest class

---
+++ src/test/java/com/example/shop/controller/UserControllerTest.java
@@ -0,0 +1,25 @@
+package com.example.shop.controller;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@SpringBootTest
+@Testcontainers
+public class UserControllerTest {
+
+    @Container
+    private static final MySQLContainer<?> MY_SQL_CONTAINER = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"));
+
+    @DynamicPropertySource
+    public static void dynamicProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        dynamicPropertyRegistry.add("spring.datasource.url", MY_SQL_CONTAINER::getJdbcUrl);
+        dynamicPropertyRegistry.add("spring.datasource.username", MY_SQL_CONTAINER::getUsername);
+        dynamicPropertyRegistry.add("spring.datasource.password", MY_SQL_CONTAINER::getPassword);
+
+    }
+}
