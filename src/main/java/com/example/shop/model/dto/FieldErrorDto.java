9247ac3ee12a84a8f1bc7ff1a31ce53fdbbe5f29
NBMARIASW11\Maria Stachowiak <mariastachowiak87@gmail.com>
2024-06-30 12:02:18 +0200

    fix typo

--- src/main/java/com/example/shop/model/dto/FieldErrorDto.java
+++ src/main/java/com/example/shop/model/dto/FieldErrorDto.java
@@ -3,5 +3,5 @@ package com.example.shop.model.dto;
 import lombok.Builder;
 
 @Builder
-public record FieldErrorDto(String fieldName, String massage) {
+public record FieldErrorDto(String fieldName, String message) {
 }
